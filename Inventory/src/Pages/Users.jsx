dedb97dd524fdab76415360643d79790493a62e2
2AhmedEissa <ahmadoeissa@gmail.com>
2026-01-06 11:02:15 +0200

    Update Users.jsx

--- Inventory/src/Pages/Users.jsx
+++ Inventory/src/Pages/Users.jsx
@@ -2,8 +2,8 @@ import { useUsers } from "../hooks/useUsers";
 import UserSearch from "../components/Users/UserSearch";
 import { UserList } from "../components/Users/UserList";
 import UserDetails from "../components/Users/UserDetails";
-import { Skeleton } from "../components/Shared/Skeleton";
-import { Users as UsersIcon, AlertCircle, Sparkles, Plus } from "lucide-react";
+import { Skeleton } from "../components/Shared/skeleton";
+import { Users as UsersIcon, AlertCircle, Sparkles} from "lucide-react";
 import AddButton from "../components/Shared/AddButton";
 import UserModal from "../components/Users/UserModal";
 
